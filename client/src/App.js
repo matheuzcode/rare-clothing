7361a3608da3d262ea36f4bb5b16474c3f9174df
Matheus Monteiro <matheus_wellyso2015@hotmail.com>
2023-03-04 01:01:05 -0300

    add client folder

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import { useEffect } from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
+
+import Home from './scenes/home/Home.jsx'
+import ItemDetails from './scenes/itemsDetails/ItemDetails.jsx'
+import Checkout from './scenes/itemsDetails/ItemDetails.jsx'
+import Confirmation from './scenes/checkout/Confirmation.jsx'
+
+import Navbar from './scenes/global/Navbar.jsx'
+
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname])
+
+  return null;
+}
+
+function App() {
+  return (
+    <div className="app">
+     <BrowserRouter>
+       <Navbar/>     
+       <ScrollToTop />
+       <Routes>
+         <Route path="/" element={<Home/>} />
+         <Route path="item/:itemId" element={<ItemDetails/>} />
+         <Route path="checkout" element={<Checkout/>} />
+         <Route path="checkout/success" element={<Confirmation/>} />
+       </Routes>
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
