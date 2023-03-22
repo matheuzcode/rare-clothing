1f85d7e6631519d0f3393021a92b988502b42014
Matheus Monteiro <matheus_wellyson2015@hotmail.com>
2023-03-22 16:57:19 -0300

    product page design

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { useState } from 'react'
 import './Product.scss'
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import BalanceIcon from "@mui/icons-material/Balance";
 
 const Product = () => {
 	const [selectedImg, setSelectedImg] = useState(0);
-
+	const [quantity, setQuantity] = useState(1);
 	const images = [
 		"https://images.pexels.com/photos/9286361/pexels-photo-9286361.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 		"https://images.pexels.com/photos/9286315/pexels-photo-9286315.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -23,12 +26,38 @@ const Product = () => {
 			</div>
 			<div className='right'>
 				<h1>Title</h1>
-				<span>$199</span>
+				<span className='price'>$19.9</span>
 				<p>Lkdosddoskdosk okd oskdosdko ksodk sodkoskdo ksdok oskdo ksodk oskdoksosokosko odksoksoksoksokd</p>
 				<div className='quantity'>
-					<button>-</button>
-					<button>+</button>
+					<button onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}>-</button>
+					{quantity}
+					<button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
 				</div>
+				<button className='add'>
+					<AddShoppingCartIcon /> 
+					<div>ADD TO CART</div>
+				</button>
+				<div className='links'>
+					<div className='item'>
+						<FavoriteBorderIcon /> ADD TO WISH LIST
+					</div>
+					<div className='item'>
+						<BalanceIcon /> ADD TO COMPARE
+					</div>
+				</div>
+				<div className="info">
+	              <span>Vendor: Polo</span>
+	              <span>Product Type: T-Shirt</span>
+	              <span>Tag: T-Shirt, Women, Top</span>
+	            </div>
+            	<hr />
+	            <div className="info">
+	              <span>DESCRIPTION</span>
+	              <hr />
+	              <span>ADDITIONAL INFORMATION</span>
+	              <hr />
+	              <span>FAQ</span>
+	            </div>
 			</div>
 		</div>	
 	)
