0d00ffe1cf1e59733ba2ca95b969aeecc77ae4a3
Matheus Monteiro <matheus_wellyson2015@hotmail.com>
2023-04-02 23:32:43 -0300

    single product page

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -4,24 +4,26 @@ import './Product.scss'
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import BalanceIcon from "@mui/icons-material/Balance";
+import { useParams } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch.js';
 
 const Product = () => {
-	const [selectedImg, setSelectedImg] = useState(0);
+	const id = useParams().id;
+	const [selectedImg, setSelectedImg] = useState("img");
 	const [quantity, setQuantity] = useState(1);
-	const images = [
-		"https://images.pexels.com/photos/9286361/pexels-photo-9286361.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-		"https://images.pexels.com/photos/9286315/pexels-photo-9286315.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-	];
+	
+	const {data, loading, error } = useFetch(`/products/${id}?populate=*`);
 
 	return (
 		<div className='product'>
+			{loading ? "loading" : (<>
 			<div className='left'>
 				<div className='images'>
-					<img src={images[0]} alt="" onClick={e=>setSelectedImg(0)}/>
-					<img src={images[1]} alt="" onClick={e=>setSelectedImg(1)}/>
+					<img src={process.env.REACT_APP_UPLOAD_URL +data?.attributes?.img?.data?.attributes?.url} alt="" onClick={e=>setSelectedImg("img")}/>
+					<img src={process.env.REACT_APP_UPLOAD_URL +data?.attributes?.img2?.data?.attributes?.url} alt="" onClick={e=>setSelectedImg("img2")}/>
 				</div>
 				<div className='mainImg'>
-					<img src={images[selectedImg]} alt="" />
+					<img src={process.env.REACT_APP_UPLOAD_URL +data?.attributes[selectedImg]?.data?.attributes?.url} alt="" />
 				</div>
 			</div>
 			<div className='right'>
@@ -59,6 +61,8 @@ const Product = () => {
 	              <span>FAQ</span>
 	            </div>
 			</div>
+			</>
+			)}
 		</div>	
 	)
 }
